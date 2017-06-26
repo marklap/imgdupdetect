b422b4c29d987c6b0f63e66dea77224c6236988b
lapem001 <mark.a.laperriere@disney.com>
2017-06-25 23:33:00 -0700

    adding datastore Add

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -20,13 +20,13 @@ type Datastorer interface {
 	Close() error
 
 	// Get gets the set of fileData that has the same fingerprint.
-	Get(collection string, fingerprint []byte) ([][]byte, error)
+	Get(collection string, fingerprint []byte) (map[string][]map[string][]byte, error)
 
-	// Add adds fileData for a fingerprint.
-	Add(collection string, fingerprint []byte, fileData []byte) error
+	// Add adds a file data for a fingerprint.
+	Add(collection string, fingerprint []byte, filename string, data map[string][]byte) error
 
-	// Remove removes fileData for a fingerprint.
-	Remove(collection string, fingerprint []byte, fileData []byte) error
+	// Remove removes a file from the set of files for this fingerprint.
+	Remove(collection string, fingerprint []byte, filename string) error
 }
 
 // Datastore is the default implementation of a Datastorer.
@@ -53,17 +53,61 @@ func (d *Datastore) Close() error {
 	return d.db.Close()
 }
 
-// Get gets the file data associated with this fingerprint.
-func (d *Datastore) Get(col string, fp []byte) ([][]byte, error) {
+// Get gets the set of file data associated with this fingerprint.
+func (d *Datastore) Get(col string, fp []byte) (map[string][]map[string][]byte, error) {
+	// var res map[string][]map[string][]byte
+	// var err error
+	// err = d.db.View(func(tx *bolt.Tx) error {
+	// 	root := tx.Bucket([]byte(col))
+	// 	if root == nil {
+	// 		return nil
+	// 	}
+	// 	bkt := root.Bucket(fp)
+	// 	if bkt == nil {
+	// 		return nil
+	// 	}
+	// 	res = make(map[string][]map[string][]byte)
+	// 	bkt.ForEach(func(k, v []byte) error {
+	// 		var m = make(map[string][]byte)
+	// 		b.ForEach(func(k, v []byte) error {
+	// 			m[string(k)] = v
+	// 		})
+	// 		res[string(n)] = append(res[string(n)], m)
+	// 	})
+	// })
+	// return res, err
 	return nil, fmt.Errorf("not yet implemented")
 }
 
 // Add adds file data to the set of file data associated with this fingerprint.
-func (d *Datastore) Add(col string, fp []byte, fileData []byte) error {
-	return fmt.Errorf("not yet implemented")
+func (d *Datastore) Add(col string, fp []byte, name string, data map[string][]byte) error {
+	var err error
+	err = d.db.Update(func(tx *bolt.Tx) error {
+		cBkt, err := tx.CreateBucketIfNotExists([]byte(col))
+		if err != nil {
+			return err
+		}
+
+		fpBkt, err := cBkt.CreateBucketIfNotExists(fp)
+		if err != nil {
+			return err
+		}
+
+		fileBkt, err := fpBkt.CreateBucketIfNotExists([]byte(name))
+		if err != nil {
+			return err
+		}
+
+		for k, v := range data {
+			fileBkt.Put([]byte(k), v)
+		}
+
+		return nil
+	})
+	return err
 }
 
 // Remove removes a particular file from the set of files associated with this fingerprint.
-func (d *Datastore) Remove(col string, fp []byte, data []byte) error {
+func (d *Datastore) Remove(col string, fp []byte, name string) error {
 	return fmt.Errorf("not yet implemented")
 }
--- datastore/datastore_test.go
+++ datastore/datastore_test.go
@@ -1,1 +1,21 @@
 package datastore
+
+import (
+	"testing"
+)
+
+var (
+	tstDatastorePath = "./testdata.dstore"
+)
+
+func TestDataStoreAdd(t *testing.T) {
+	ds, err := Open(Config{tstDatastorePath})
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = ds.Add("test", []byte{1, 2, 3}, "/tmp/my/file/name.jpg", map[string][]byte{"key": []byte("value")})
+	if err != nil {
+		t.Error(err)
+	}
+}
